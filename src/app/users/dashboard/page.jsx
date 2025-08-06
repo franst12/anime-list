ed31c375d48f908a253c7b08810bc2ebb4d5bfd1
Drajet Franst <fransye12345@gmail.com>
2025-08-06 10:20:14 +0700

    sesi8 berakhir disini dengan tugasnya juga

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -4,17 +4,14 @@ import React from 'react';
 
 const page = async () => {
   const user = await authUsereSession();
-  const PageDashboard = () => {
-    return (
-      <div>
-        <h1>Dashboard</h1>
-        <p>Hello, {user?.name}</p>
-        <Image src={user?.image} alt={user?.name} width={250} height={250} />
-      </div>
-    );
-  };
 
-  return <>{user ? <PageDashboard /> : <h1>Anda Belum Login</h1>}</>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Hello, {user?.name}</p>
+      <Image src={user?.image} alt={user?.name} width={250} height={250} />
+    </div>
+  );
 };
 
 export default page;
